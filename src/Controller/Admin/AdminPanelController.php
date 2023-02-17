617bf8cae02443c49efbbcb0b20880c49e06a7ff
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 <71237769+sayedbenslimene@users.noreply.github.com>
2023-02-17 12:40:54 +0100

    first commit

--- src/Controller/Admin/AdminPanelController.php
+++ src/Controller/Admin/AdminPanelController.php
@@ -8,12 +8,28 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class AdminPanelController extends AbstractController
 {
-    /**
-     * @Route("/admin")
-     * @return Response
-     */
+    #[Route('/admin', name: 'app_admin')]
+
     public function index(): Response
     {
         return $this->render('admin_panel/admin.html.twig');
     }
+    #[Route('/loginAuth', name: 'Auth_login')]
+    public function loginAuth(): Response
+    {
+        return $this->render('admin_panel/loginAuth.html.twig');
+    }
+    #[Route('/registerAuth', name: 'Auth_register')]
+    public function registerAuth(): Response
+    {
+        return $this->render('admin_panel/registerAuth.html.twig');
+    }
+    #[Route('/Auth_forgot_pass', name: 'Auth_forgot_pass')]
+    public function forgot_passAuth(): Response
+    {
+        return $this->render('admin_panel/Auth_forgot_pass.html.twig');
+    }
+
+
+
 }
