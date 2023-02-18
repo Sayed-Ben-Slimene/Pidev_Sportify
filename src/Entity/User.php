2a68edb8e874bf5ffe4e66a2dbea025a75f3ed27
Sayed-Ben-Slimene <sayed.benslimene@esprit.tn>
2023-02-18 23:16:50 +0100

    first commit

---
+++ src/Entity/User.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+class User
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    #[Assert\NotBlank(message: "vous devez mettre votre nom!!!")]
+    private ?string $nom = null;
+
+    #[ORM\Column(length: 255)]
+    #[Assert\NotBlank(message: "vous devez mettre votre prenom!!!")]
+    private ?string $prenom = null;
+
+    #[ORM\Column]
+    #[Assert\NotBlank(message: "vous devez mettre votre telephone!!!")]
+    private ?int $tel = null;
+
+    #[ORM\Column(length: 255)]
+    #[Assert\NotBlank(message: "vous devez mettre votre adresse!!!")]
+    private ?string $adress = null;
+
+    #[ORM\Column(length: 255)]
+    #[Assert\NotBlank(message: 'The email {{ value }} is not a valid email.',)]
+    private ?string $email = null;
+
+    #[ORM\Column(length: 255)]
+    #[Assert\NotBlank(message: "vous devez mettre votre mot de passe!!!")]
+    private ?string $password = null;
+
+    #[ORM\Column(type: Types::OBJECT)]
+    #[Assert\NotBlank(message: "vous devez mettre votre role(Admin ou Organisateur)!!!")]
+    private ?string $role = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getPrenom(): ?string
+    {
+        return $this->prenom;
+    }
+
+    public function setPrenom(string $prenom): self
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    public function getTel(): ?int
+    {
+        return $this->tel;
+    }
+
+    public function setTel(int $tel): self
+    {
+        $this->tel = $tel;
+
+        return $this;
+    }
+
+    public function getAdress(): ?string
+    {
+        return $this->adress;
+    }
+
+    public function setAdress(string $adress): self
+    {
+        $this->adress = $adress;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    public function getRole(): ?string
+    {
+        return $this->role;
+    }
+
+    public function setRole(string $role): self
+    {
+        $this->role = $role;
+
+        return $this;
+    }
+}
